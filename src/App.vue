21c9021a5d44c9185187d8e77e1617bc6293fdfd
Gavin Dreyer <gavindreyer1@gmail.com>
2020-05-21 20:39:30 -0700

    conditionally rendering list of verbs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<IndicativePresent v-bind:verbs="verbs" :loading="loading" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndicativePresent from './components/IndicativePresent.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		IndicativePresent
+	},
+	data() {
+		return {
+			verbs: [],
+			loading: false
+		};
+	},
+	created() {},
+	mounted() {
+		this.fetchVerbs();
+	},
+	methods: {
+		fetchVerbs: function() {
+			this.loading = true;
+			const baseURI = 'http://localhost:4444/api/tense/indicativePresent';
+			this.$http
+				.get(baseURI)
+				.then(result => {
+					this.verbs.push(result.data);
+				})
+				.catch(err => console.log(err))
+				.finally(() => {
+					this.loading = false;
+					console.log(this.loading);
+				});
+		}
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/IndicativePresent.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<div v-if="loading">Loading</div>
+		<div v-else>{{ verbs }}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			help: 'help'
+		};
+	},
+	props: {
+		verbs: Array,
+		loading: Boolean
+	},
+	methods: {
+		splitter() {
+			console.log(this.verbs);
+			return this.verbs[0][0];
+		}
+	},
+	computed: {}
+};
+</script>
+
+<style></style>
