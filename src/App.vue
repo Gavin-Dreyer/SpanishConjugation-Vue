93d75c2538a108610e800caa8f3cd3aa0f03ec9d
Gavin Dreyer <gavindreyer1@gmail.com>
2020-05-22 15:25:43 -0700

    submitting the correct firstPersonSingular conjugation adds a point and rerenders the IndicativePresent component for the next question

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,63 @@
 <template>
-	<div id="app">
-		<IndicativePresent v-bind:verbs="verbs" :loading="loading" />
-	</div>
+  <div id="app">
+    <IndicativePresent
+      v-bind:verbs="verbs"
+      :loading="loading"
+      :reRender="this.reRender"
+      :points="points"
+      :key="componentKey"
+    />
+  </div>
 </template>
 
 <script>
-import IndicativePresent from './components/IndicativePresent.vue';
+import IndicativePresent from "./components/IndicativePresent.vue";
 
 export default {
-	name: 'App',
-	components: {
-		IndicativePresent
-	},
-	data() {
-		return {
-			verbs: [],
-			loading: false
-		};
-	},
-	mounted() {
-		this.fetchVerbs();
-	},
-	methods: {
-		fetchVerbs: function() {
-			this.loading = true;
-			const baseURI = 'http://localhost:4444/api/tense/indicativePresent';
-			this.$http
-				.get(baseURI)
-				.then(result => {
-					this.verbs.push(result.data);
-				})
-				.catch(err => console.log(err))
-				.finally(() => {
-					this.loading = false;
-				});
-		}
-	}
+  name: "App",
+  components: {
+    IndicativePresent
+  },
+  data() {
+    return {
+      verbs: [],
+      loading: false,
+      points: 0,
+      componentKey: 0
+    };
+  },
+  mounted() {
+    this.fetchVerbs();
+  },
+  methods: {
+    fetchVerbs: function() {
+      this.loading = true;
+      const baseURI = "http://localhost:4444/api/tense/indicativePresent";
+      this.$http
+        .get(baseURI)
+        .then(result => {
+          this.verbs.push(result.data);
+        })
+        .catch(err => console.log(err))
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    reRender: function() {
+      this.componentKey++;
+      this.points++;
+    }
+  }
 };
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/components/IndicativePresent.vue
+++ src/components/IndicativePresent.vue
@@ -1,21 +1,46 @@
 <template>
-	<div>
-		<div v-if="loading">Loading</div>
-		<div v-else>{{ verbs }}</div>
-	</div>
+  <div>
+    <div v-if="loading">Loading</div>
+    <div v-else-if="verbs.length > 0">
+      <p>{{ randomVerb.spanishVerb }}</p>
+      <p>{{ points }}</p>
+    </div>
+    <form id="inputForm">
+      <p>
+        <input type="text" placeholder="Answer" v-model="answer" />
+      </p>
+      <button v-on:click="submit">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {};
-	},
-	props: {
-		verbs: Array,
-		loading: Boolean
-	},
-	methods: {},
-	computed: {}
+  data() {
+    return {
+      answer: ""
+    };
+  },
+  props: {
+    verbs: Array,
+    loading: Boolean,
+    points: Number,
+    reRender: Function
+  },
+  created() {},
+  mounted() {},
+  computed: {
+    randomVerb() {
+      if (this.verbs.length === 0) return [];
+      return this.verbs[0][Math.floor(Math.random() * this.verbs[0].length)];
+    }
+  },
+  methods: {
+    submit: function(evt) {
+      evt.preventDefault();
+      if (this.answer === this.randomVerb.firstPersonSingular) this.reRender();
+    }
+  }
 };
 </script>
 
