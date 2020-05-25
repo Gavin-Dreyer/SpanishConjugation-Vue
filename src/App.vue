286b14756dda6c500c8d775b5fd9d65c0df660b5
Gavin Dreyer <gavindreyer1@gmail.com>
2020-05-24 21:07:24 -0700

    added html and css to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,150 @@
 <template>
-  <div id="app">
-    <IndicativePresent
-      v-bind:verbs="verbs"
-      :loading="loading"
-      :reRender="this.reRender"
-      :points="points"
-      :key="componentKey"
-    />
-
-    <input type="checkbox" id="present" value="Presente" v-model="checkedTenses" />
-    <label for="present">Present</label>
-    <input type="checkbox" id="past" value="Pretérito" v-model="checkedTenses" />
-    <label for="past">Past</label>
-    <input type="checkbox" id="imperfect" value="Imperfecto" v-model="checkedTenses" />
-    <label for="imperfect">Imperfect</label>
-    <input type="checkbox" id="future" value="Futuro" v-model="checkedTenses" />
-    <label for="future">Future</label>
-    <button v-on:click="fetchTenses">Fetch Selected Tenses</button>
-  </div>
+	<div id="app">
+		<IndicativePresent
+			v-bind:verbs="verbs"
+			:loading="loading"
+			:reRender="this.reRender"
+			:points="points"
+			:key="componentKey"
+		/>
+		<div class="tensesCheckbox">
+			<div class="checkBoxCon">
+				<div class="checkBoxes">
+					<input
+						type="checkbox"
+						id="present"
+						value="Presente"
+						v-model="checkedTenses"
+					/>
+					<label for="present">Present</label>
+				</div>
+				<div class="checkBoxes">
+					<input
+						type="checkbox"
+						id="past"
+						value="Pretérito"
+						v-model="checkedTenses"
+					/>
+					<label for="past">Past</label>
+				</div>
+				<div class="checkBoxes">
+					<input
+						type="checkbox"
+						id="imperfect"
+						value="Imperfecto"
+						v-model="checkedTenses"
+					/>
+					<label for="imperfect">Imperfect</label>
+				</div>
+				<div class="checkBoxes">
+					<input
+						type="checkbox"
+						id="future"
+						value="Futuro"
+						v-model="checkedTenses"
+					/>
+					<label for="future">Future</label>
+				</div>
+			</div>
+			<button v-on:click="fetchTenses">Fetch Selected Tenses</button>
+		</div>
+	</div>
 </template>
 
 <script>
-import IndicativePresent from "./components/IndicativePresent.vue";
+import IndicativePresent from './components/IndicativePresent.vue';
 
 export default {
-  name: "App",
-  components: {
-    IndicativePresent
-  },
-  data() {
-    return {
-      verbs: [],
-      checkedTenses: [],
-      loading: false,
-      points: 0,
-      componentKey: 0
-    };
-  },
-  mounted() {
-    this.fetchVerbs();
-  },
-  methods: {
-    fetchVerbs: function() {
-      this.loading = true;
-      const baseURI = "http://localhost:4444/api/tense/indicativePresent";
-      this.$http
-        .get(baseURI)
-        .then(result => {
-          this.verbs = result.data;
-        })
-        .catch(err => console.log(err))
-        .finally(() => {
-          this.loading = false;
-        });
-    },
-    fetchTenses: function() {
-      if (this.checkedTenses.length === 0) return;
+	name: 'App',
+	components: {
+		IndicativePresent
+	},
+	data() {
+		return {
+			verbs: [],
+			checkedTenses: [],
+			loading: false,
+			points: 0,
+			componentKey: 0
+		};
+	},
+	mounted() {
+		this.fetchVerbs();
+	},
+	methods: {
+		fetchVerbs: function() {
+			this.loading = true;
+			const baseURI = 'http://localhost:4444/api/tense/indicativePresent';
+			this.$http
+				.get(baseURI)
+				.then(result => {
+					this.verbs = result.data;
+				})
+				.catch(err => console.log(err))
+				.finally(() => {
+					this.loading = false;
+				});
+		},
+		fetchTenses: function() {
+			if (this.checkedTenses.length === 0) return;
 
-      this.loading = true;
-      const baseURI = "http://localhost:4444/api/tense/multiTense";
-      this.$http
-        .get(baseURI, { params: { tenses: this.checkedTenses } })
-        .then(result => {
-          this.verbs = result.data;
-        })
-        .catch(err => console.log(err))
-        .finally(() => {
-          this.loading = false;
-        });
-    },
-    reRender: function() {
-      this.componentKey++;
-      this.points++;
-    }
-  }
+			this.loading = true;
+			const baseURI = 'http://localhost:4444/api/tense/multiTense';
+			this.$http
+				.get(baseURI, { params: { tenses: this.checkedTenses } })
+				.then(result => {
+					this.verbs = result.data;
+				})
+				.catch(err => console.log(err))
+				.finally(() => {
+					this.loading = false;
+				});
+		},
+		reRender: function() {
+			this.componentKey++;
+			this.points++;
+		}
+	}
 };
 </script>
 
 <style>
+html {
+	height: 100%;
+}
+
+body {
+	height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.tensesCheckbox {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	align-items: center;
+	width: 50%;
+	height: 10%;
+}
+
+.checkBoxCon {
+	display: flex;
+	justify-content: space-around;
+	width: 100%;
+}
+
+.checkBoxes label {
+	margin-left: 0.1rem;
 }
 </style>
--- src/components/IndicativePresent.vue
+++ src/components/IndicativePresent.vue
@@ -1,82 +1,91 @@
 <template>
-  <div>
-    <div v-if="loading">Loading</div>
-    <div v-else-if="verbs.length > 0">
-      <p>Tense: {{randomVerb.tense}}</p>
-      <p>Mood: {{randomVerb.mood}}</p>
-      <p>Conjugate {{ randomVerb.spanishVerb }} into {{ randomVerb.view }}</p>
-      <p>{{ points }}</p>
-    </div>
-    <form id="inputForm">
-      <p>
-        <input type="text" placeholder="Answer" v-model="answer" />
-      </p>
-      <button v-on:click="submit">Submit</button>
-    </form>
-  </div>
+	<div>
+		<div v-if="loading">Loading</div>
+		<div v-else-if="verbs.length > 0">
+			<p>
+				Conjugate
+				<span id="spanishVerb">{{ randomVerb.spanishVerb }}</span>
+				into {{ randomVerb.view }}
+			</p>
+			<p>Tense: {{ randomVerb.tense }}</p>
+			<p>Mood: {{ randomVerb.mood }}</p>
+			<p>{{ points }}</p>
+		</div>
+		<form id="inputForm">
+			<p>
+				<input type="text" placeholder="Answer" v-model="answer" />
+			</p>
+			<button v-on:click="submit">Submit</button>
+		</form>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      answer: ""
-    };
-  },
-  props: {
-    verbs: Array,
-    loading: Boolean,
-    points: Number,
-    reRender: Function
-  },
-  created() {},
-  mounted() {},
-  computed: {
-    randomVerb() {
-      if (this.verbs.length === 0) return [];
+	data() {
+		return {
+			answer: ''
+		};
+	},
+	props: {
+		verbs: Array,
+		loading: Boolean,
+		points: Number,
+		reRender: Function
+	},
+	created() {},
+	mounted() {},
+	computed: {
+		randomVerb() {
+			if (this.verbs.length === 0) return [];
 
-      let v = [];
-      console.log(this.verbs);
-      //   Randomly selects a verb from the indicative present verbs
-      let verb = this.verbs[Math.floor(Math.random() * this.verbs.length)];
+			let v = [];
 
-      //creates a list of the 6 different points of view
-      Object.keys(verb).forEach((item, index) => {
-        if (index > 4) {
-          v.push({ view: item, conjugation: verb[item] });
-        }
-      });
+			//   Randomly selects a verb from the indicative present verbs
+			let verb = this.verbs[Math.floor(Math.random() * this.verbs.length)];
 
-      //randomly select a view and format it
-      v = v[Math.floor(Math.random() * v.length)];
-      v["view"] = v["view"]
-        .split("")
-        .map((letter, index) => {
-          if (index === 0) {
-            return (letter = letter.toUpperCase());
-          } else if (letter.match(/[A-Z]/)) {
-            return (letter = " " + letter);
-          } else {
-            return letter;
-          }
-        })
-        .join("");
+			//creates a list of the 6 different points of view
+			Object.keys(verb).forEach((item, index) => {
+				if (index > 4) {
+					v.push({ view: item, conjugation: verb[item] });
+				}
+			});
 
-      return {
-        spanishVerb: verb.spanishVerb,
-        tense: verb.tense,
-        mood: verb.mood,
-        ...v
-      };
-    }
-  },
-  methods: {
-    submit: function(evt) {
-      evt.preventDefault();
-      if (this.answer === this.randomVerb.conjugation) this.reRender();
-    }
-  }
+			//randomly select a view and format it
+			v = v[Math.floor(Math.random() * v.length)];
+			v['view'] = v['view']
+				.split('')
+				.map((letter, index) => {
+					if (index === 0) {
+						return (letter = letter.toUpperCase());
+					} else if (letter.match(/[A-Z]/)) {
+						return (letter = ' ' + letter);
+					} else {
+						return letter;
+					}
+				})
+				.join('');
+
+			return {
+				spanishVerb: verb.spanishVerb,
+				tense: verb.tense,
+				mood: verb.mood,
+				...v
+			};
+		}
+	},
+	methods: {
+		submit: function(evt) {
+			evt.preventDefault();
+			if (this.answer === this.randomVerb.conjugation) this.reRender();
+		}
+	}
 };
 </script>
 
-<style></style>
+<style>
+#spanishVerb {
+	font-weight: 700;
+	font-size: 1.25rem;
+}
+</style>
